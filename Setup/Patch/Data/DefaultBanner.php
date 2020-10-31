ce53d181508287bc165062c5a63ce16f16a6899b
zengliwei <zengliwei@163.com>
2020-10-31 14:31:38 +0800

    ^ upgrade setup script

--- Setup/Patch/Data/DefaultBanner.php
+++ Setup/Patch/Data/DefaultBanner.php
@@ -29,6 +29,8 @@ use Magento\Framework\App\State;
 use Magento\Framework\Filesystem;
 use Magento\Framework\Setup\Patch\DataPatchInterface;
 use Magento\Framework\View\Asset\Repository as AssetRepository;
+use Magento\Widget\Model\ResourceModel\Widget\Instance as ResourceWidget;
+use Magento\Widget\Model\Widget\InstanceFactory as WidgetFactory;
 
 /**
  * @package Common\Banner
@@ -58,6 +60,11 @@ class DefaultBanner implements DataPatchInterface
     private $itemFactory;
 
     /**
+     * @var WidgetFactory
+     */
+    private $widgetFactory;
+
+    /**
      * @var ResourceGroup
      */
     private $resourceGroup;
@@ -66,6 +73,11 @@ class DefaultBanner implements DataPatchInterface
      * @var ResourceItem
      */
     private $resourceItem;
+
+    /**
+     * @var ResourceWidget
+     */
+    private $resourceWidget;
 
     /**
      * @var State
@@ -78,8 +90,10 @@ class DefaultBanner implements DataPatchInterface
      * @param Filesystem      $filesystem
      * @param GroupFactory    $groupFactory
      * @param ItemFactory     $itemFactory
+     * @param WidgetFactory   $widgetFactory
      * @param ResourceGroup   $resourceGroup
      * @param ResourceItem    $resourceItem
+     * @param ResourceWidget  $resourceWidget
      */
     public function __construct(
         State $state,
@@ -87,15 +101,19 @@ class DefaultBanner implements DataPatchInterface
         Filesystem $filesystem,
         GroupFactory $groupFactory,
         ItemFactory $itemFactory,
+        WidgetFactory $widgetFactory,
         ResourceGroup $resourceGroup,
-        ResourceItem $resourceItem
+        ResourceItem $resourceItem,
+        ResourceWidget $resourceWidget
     ) {
         $this->assetRepository = $assetRepository;
         $this->filesystem = $filesystem;
         $this->groupFactory = $groupFactory;
         $this->itemFactory = $itemFactory;
+        $this->widgetFactory = $widgetFactory;
         $this->resourceGroup = $resourceGroup;
         $this->resourceItem = $resourceItem;
+        $this->resourceWidget = $resourceWidget;
         $this->state = $state;
     }
 
@@ -114,8 +132,8 @@ class DefaultBanner implements DataPatchInterface
     {
         $groupSource = [
             [
-                'data'  => ['identifier' => 'home', 'name' => 'Home Banner'],
-                'items' => [
+                'data'   => ['identifier' => 'home', 'name' => 'Home Banner'],
+                'items'  => [
                     [
                         'title'   => 'Home Banner 01',
                         'type'    => 'image',
@@ -142,32 +160,53 @@ class DefaultBanner implements DataPatchInterface
                         'media' => 'home-banner-04.mp4',
                         'order' => 4
                     ]
+                ],
+                'widget' => [
+                    'instance_type'     => 'Common\Banner\Block\Widget\Carousel',
+                    'theme_id'          => 3, // todo :: change
+                    'title'             => 'Home Banner',
+                    'store_ids'         => [0],
+                    'widget_parameters' => '{"group_id":"1","adaptive_height":"true","arrows":"true","autoplay":"true","autoplay_speed":"3000","center_mode":"false","center_padding":"50px","dots":"true","draggable":"true","fade":"false","focus_on_change":"false","focus_on_select":"false","infinite":"true","initial_slide":"0","lazy_load":"ondemand","pause_on_dots_hover":"false","pause_on_focus":"true","pause_on_hover":"true","accessibility":"true","rows":"1","rtl":"false","slides_per_row":"1","slides_to_scroll":"1","slides_to_show":"1","speed":"300","swipe":"true","swipe_to_slide":"false","touch_move":"true","touch_threshold":"5","variable_width":"false","vertical":"false","vertical_swiping":"false","wait_for_animate":"true"}',
+                    'page_groups'       => [
+                        [
+                            'page_group' => 'pages',
+                            'pages'      => [
+                                'page_id'       => null,
+                                'group'         => 'pages',
+                                'layout_handle' => 'cms_index_index',
+                                'block'         => 'content',
+                                'for'           => 'all',
+                                'template'      => 'group/widget/carousel.phtml'
+                            ]
+                        ]
+                    ]
                 ]
             ]
         ];
-        $this->state->emulateAreaCode(
-            Area::AREA_CRONTAB,
-            function () use ($groupSource) {
-                $mediaDir = $this->filesystem->getDirectoryWrite(DirectoryList::MEDIA)->getAbsolutePath()
-                    . Item::MEDIA_FOLDER;
-                if (!is_dir($mediaDir)) {
-                    mkdir($mediaDir, 0775, true);
-                }
-                foreach ($groupSource as $groupData) {
-                    $group = $this->groupFactory->create();
-                    $this->resourceGroup->save($group->setData($groupData['data']));
-                    foreach ($groupData['items'] as $itemData) {
-                        $itemData['group_id'] = $group->getId();
-                        $item = $this->itemFactory->create();
-                        $this->resourceItem->save($item->setData($itemData));
-                        if (!is_file($mediaDir . '/' . $itemData['media'])) {
-                            $asset = $this->assetRepository->createAsset('Common_Banner::images/' . $itemData['media']);
-                            copy($asset->getSourceFile(), $mediaDir . '/' . $itemData['media']);
-                        }
-                    }
+        $mediaDir = $this->filesystem->getDirectoryWrite(DirectoryList::MEDIA)->getAbsolutePath() . Item::MEDIA_FOLDER;
+        if (!is_dir($mediaDir)) {
+            mkdir($mediaDir, 0775, true);
+        }
+        foreach ($groupSource as $groupData) {
+            $group = $this->groupFactory->create();
+            $this->resourceGroup->save($group->setData($groupData['data']));
+            foreach ($groupData['items'] as $itemData) {
+                $itemData['group_id'] = $group->getId();
+                $item = $this->itemFactory->create();
+                $this->resourceItem->save($item->setData($itemData));
+                if (!is_file($mediaDir . '/' . $itemData['media'])) {
+                    $asset = $this->assetRepository->createAsset('Common_Banner::images/' . $itemData['media']);
+                    copy($asset->getSourceFile(), $mediaDir . '/' . $itemData['media']);
                 }
             }
-        );
+            $this->state->emulateAreaCode(
+                Area::AREA_FRONTEND,
+                function () use ($groupData) {
+                    $widget = $this->widgetFactory->create();
+                    $this->resourceWidget->save($widget->setData($groupData['widget']));
+                }
+            );
+        }
     }
 
     /**
