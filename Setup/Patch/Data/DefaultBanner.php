3d86fcd826f3ccd892cea741b3cdeebe233ab75c
zengliwei <zengliwei@163.com>
2020-10-31 08:36:31 +0800

    + add sample data

--- Setup/Patch/Data/DefaultBanner.php
+++ Setup/Patch/Data/DefaultBanner.php
@@ -18,11 +18,17 @@
 
 namespace Common\Banner\Setup\Patch\Data;
 
+use Common\Banner\Model\Group\Item;
 use Common\Banner\Model\Group\ItemFactory;
 use Common\Banner\Model\GroupFactory;
 use Common\Banner\Model\ResourceModel\Group as ResourceGroup;
 use Common\Banner\Model\ResourceModel\Group\Item as ResourceItem;
+use Magento\Framework\App\Area;
+use Magento\Framework\App\Filesystem\DirectoryList;
+use Magento\Framework\App\State;
+use Magento\Framework\Filesystem;
 use Magento\Framework\Setup\Patch\DataPatchInterface;
+use Magento\Framework\View\Asset\Repository as AssetRepository;
 
 /**
  * @package Common\Banner
@@ -31,6 +37,16 @@ use Magento\Framework\Setup\Patch\DataPatchInterface;
  */
 class DefaultBanner implements DataPatchInterface
 {
+    /**
+     * @var AssetRepository
+     */
+    private $assetRepository;
+
+    /**
+     * @var Filesystem
+     */
+    private $filesystem;
+
     /**
      * @var GroupFactory
      */
@@ -52,21 +68,35 @@ class DefaultBanner implements DataPatchInterface
     private $resourceItem;
 
     /**
-     * @param GroupFactory  $groupFactory
-     * @param ItemFactory   $itemFactory
-     * @param ResourceGroup $resourceGroup
-     * @param ResourceItem  $resourceItem
+     * @var State
+     */
+    private $state;
+
+    /**
+     * @param State           $state
+     * @param AssetRepository $assetRepository
+     * @param Filesystem      $filesystem
+     * @param GroupFactory    $groupFactory
+     * @param ItemFactory     $itemFactory
+     * @param ResourceGroup   $resourceGroup
+     * @param ResourceItem    $resourceItem
      */
     public function __construct(
+        State $state,
+        AssetRepository $assetRepository,
+        Filesystem $filesystem,
         GroupFactory $groupFactory,
         ItemFactory $itemFactory,
         ResourceGroup $resourceGroup,
         ResourceItem $resourceItem
     ) {
+        $this->assetRepository = $assetRepository;
+        $this->filesystem = $filesystem;
         $this->groupFactory = $groupFactory;
         $this->itemFactory = $itemFactory;
         $this->resourceGroup = $resourceGroup;
         $this->resourceItem = $resourceItem;
+        $this->state = $state;
     }
 
     /**
@@ -89,25 +119,49 @@ class DefaultBanner implements DataPatchInterface
                     [
                         'title'   => 'Home Banner 01',
                         'type'    => 'image',
-                        'media'   => 'home-01.jpg',
+                        'media'   => 'home-banner-01.jpg',
                         'url'     => 'collections/yoga-new.html',
                         'content' => '<div class="content bg-white"> <span class="info">New Luma Yoga Collection</span> <strong class="title">Get fit and look fab in new seasonal styles</strong> <span class="action more button">Shop New Yoga</span> </span></div>',
                         'order'   => 1
                     ],
-                    ['title' => 'Home Banner 02', 'type' => 'image', 'order' => 2],
-                    ['title' => 'Home Banner 03', 'type' => 'image', 'order' => 3]
+                    [
+                        'title' => 'Home Banner 02',
+                        'type'  => 'image',
+                        'media' => 'home-banner-02.jpg',
+                        'order' => 2
+                    ],
+                    [
+                        'title' => 'Home Banner 03',
+                        'type'  => 'image',
+                        'media' => 'home-banner-03.jpg',
+                        'order' => 3
+                    ]
                 ]
             ]
         ];
-        foreach ($groupSource as $groupData) {
-            $group = $this->groupFactory->create();
-            $this->resourceGroup->save($group->setData($groupData['data']));
-            foreach ($groupData['items'] as $itemData) {
-                $itemData['group_id'] = $group->getId();
-                $item = $this->itemFactory->create();
-                $this->resourceItem->save($item->setData($itemData));
+        $this->state->emulateAreaCode(
+            Area::AREA_CRONTAB,
+            function () use ($groupSource) {
+                $mediaDir = $this->filesystem->getDirectoryWrite(DirectoryList::MEDIA)->getAbsolutePath()
+                    . Item::MEDIA_FOLDER;
+                if (!is_dir($mediaDir)) {
+                    mkdir($mediaDir, 0775, true);
+                }
+                foreach ($groupSource as $groupData) {
+                    $group = $this->groupFactory->create();
+                    $this->resourceGroup->save($group->setData($groupData['data']));
+                    foreach ($groupData['items'] as $itemData) {
+                        $itemData['group_id'] = $group->getId();
+                        $item = $this->itemFactory->create();
+                        $this->resourceItem->save($item->setData($itemData));
+                        if (!is_file($mediaDir . '/' . $itemData['media'])) {
+                            $asset = $this->assetRepository->createAsset('Common_Banner::images/' . $itemData['media']);
+                            copy($asset->getSourceFile(), $mediaDir . '/' . $itemData['media']);
+                        }
+                    }
+                }
             }
-        }
+        );
     }
 
     /**
